b7bece14ccd3d98c41ad5341d64e271190e1d6ce
Anoriya <bouhaa7@gmail.com>
2020-05-21 16:59:10 +0100

    map Reduce job to check data flow frequency

--- src/main/java/Refinement_Layer/Spark.java
+++ src/main/java/Refinement_Layer/Spark.java
@@ -10,36 +10,40 @@ import org.apache.spark.SparkConf;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.streaming.kafka010.*;
-import scala.Tuple3;
+import scala.Tuple2;
+import org.apache.spark.api.java.function.Function2;
 
 public class Spark {
 
     public Spark() throws InterruptedException {
 
+        Function2<Integer, Integer, Integer> reduceSumFunc = (key, n) -> (key + n);
         SparkConf conf = new SparkConf().setAppName("appName");
-        JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(5000));
+        JavaStreamingContext ssc = new JavaStreamingContext(conf, new Duration(2000));
 
         Map<String, Object> kafkaParams = new HashMap<>();
         kafkaParams.put("bootstrap.servers", "quickstart.cloudera:9092");
         kafkaParams.put("key.deserializer", StringDeserializer.class);
         kafkaParams.put("value.deserializer", StringDeserializer.class);
-        kafkaParams.put("group.id", "g25");
+        kafkaParams.put("group.id", "spark");
         kafkaParams.put("auto.offset.reset", "earliest");
-        kafkaParams.put("enable.auto.commit", false);
+        kafkaParams.put("enable.auto.commit", true);
 
-        Collection<String> topics = Arrays.asList("test", "camera");
+        Collection<String> topics = Arrays.asList("Aw", "Zephyr", "Camera", "Empatica");
 
         JavaInputDStream<ConsumerRecord<String, String>> stream = KafkaUtils.createDirectStream(
                 ssc,
-                LocationStrategies.PreferConsistent(),
+                LocationStrategies.PreferBrokers(),
                 ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
         );
 
-        JavaDStream<Tuple3<String, Long, Long>> lines = stream.map(line -> new Tuple3<>(line.topic(),line.timestamp(),line.offset()));
+        JavaPairDStream<String, Integer> topic_pairs = stream.mapToPair(record -> new Tuple2<>(record.topic(), 1));
 
+        JavaPairDStream<String, Integer> topic_count = topic_pairs.reduceByKey(reduceSumFunc);
 
-        lines.foreachRDD(rdd -> {
-            rdd.foreachPartition(System.out::println);
+            topic_count.foreachRDD(rdd -> {
+                List<Tuple2<String, Integer>> sample = rdd.collect();
+                sample.forEach(System.out::println);
         });
 
 
