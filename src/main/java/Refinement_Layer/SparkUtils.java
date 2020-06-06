62388d7bf20c0a758c5f64198fd0f8753235ff84
Anoriya <bouhaa7@gmail.com>
2020-06-06 15:10:19 +0100

    Function 1 Scala

--- src/main/java/Refinement_Layer/SparkUtils.java
+++ src/main/java/Refinement_Layer/SparkUtils.java
@@ -48,6 +48,124 @@ public class SparkUtils implements Serializable {
         public <A> Function1<ArrayList<ArrayList<Double>>, A> andThen(Function1<Double[], A> g) {
             return null;
         }
+
+        @Override
+        public boolean apply$mcZD$sp(double v) {
+            return false;
+        }
+
+        @Override
+        public double apply$mcDD$sp(double v) {
+            return 0;
+        }
+
+        @Override
+        public float apply$mcFD$sp(double v) {
+            return 0;
+        }
+
+        @Override
+        public int apply$mcID$sp(double v) {
+            return 0;
+        }
+
+        @Override
+        public long apply$mcJD$sp(double v) {
+            return 0;
+        }
+
+        @Override
+        public void apply$mcVD$sp(double v) {
+
+        }
+
+        @Override
+        public boolean apply$mcZF$sp(float v) {
+            return false;
+        }
+
+        @Override
+        public double apply$mcDF$sp(float v) {
+            return 0;
+        }
+
+        @Override
+        public float apply$mcFF$sp(float v) {
+            return 0;
+        }
+
+        @Override
+        public int apply$mcIF$sp(float v) {
+            return 0;
+        }
+
+        @Override
+        public long apply$mcJF$sp(float v) {
+            return 0;
+        }
+
+        @Override
+        public void apply$mcVF$sp(float v) {
+
+        }
+
+        @Override
+        public boolean apply$mcZI$sp(int i) {
+            return false;
+        }
+
+        @Override
+        public double apply$mcDI$sp(int i) {
+            return 0;
+        }
+
+        @Override
+        public float apply$mcFI$sp(int i) {
+            return 0;
+        }
+
+        @Override
+        public int apply$mcII$sp(int i) {
+            return 0;
+        }
+
+        @Override
+        public long apply$mcJI$sp(int i) {
+            return 0;
+        }
+
+        @Override
+        public void apply$mcVI$sp(int i) {
+
+        }
+
+        @Override
+        public boolean apply$mcZJ$sp(long l) {
+            return false;
+        }
+
+        @Override
+        public double apply$mcDJ$sp(long l) {
+            return 0;
+        }
+
+        @Override
+        public float apply$mcFJ$sp(long l) {
+            return 0;
+        }
+
+        @Override
+        public int apply$mcIJ$sp(long l) {
+            return 0;
+        }
+
+        @Override
+        public long apply$mcJJ$sp(long l) {
+            return 0;
+        }
+
+        @Override
+        public void apply$mcVJ$sp(long l) {  }
     };
 
     final static Function2<Double[], Integer, Double[]> moyenne = new Function2<Double[], Integer, Double[]>() {
@@ -221,9 +339,7 @@ public class SparkUtils implements Serializable {
         }
 
         @Override
-        public void apply$mcVJ$sp(long l) {
-
-        }
+        public void apply$mcVJ$sp(long l) {  }
     };
 
     public static void process_camera(Tuple2<String, String[]> first, Iterator<Tuple2<String, String[]>> records, List<String[]> CAMERA_VECTOR){
