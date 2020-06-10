e1411f223bcbd93c8b01edc9d51d70355ee69f58
Anoriya <bouhaa7@gmail.com>
2020-06-10 15:39:59 +0100

    Test couchdb

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,14 @@
 import DBSCAN.TestDBSCAN;
 import Refinement_Layer.Spark;
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbConnector;
+import org.ektorp.impl.StdCouchDbInstance;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,6 +18,26 @@ public class main {
     public static void main(String[] args) throws InterruptedException, IOException {
 
 
-      Spark spark = new Spark();
+//      Spark spark = new Spark();
+        HttpClient httpClient = new StdHttpClient.Builder()
+                .url("http://localhost:5984").username("admin")
+                .password("Inchalah1.")
+                .build();
+        CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
+        CouchDbConnector db = new StdCouchDbConnector("test", dbInstance);
+
+        Map<String, Object> document = new HashMap<String, Object>();
+        Map<String, Object> Empatica = new HashMap<String, Object>();
+        Map<String, Object> Zephyr = new HashMap<String, Object>();
+        Map<String, Object> Acc = new HashMap<String, Object>();
+        Acc.put("val1", 60);
+        Acc.put("val2", 69);
+        Empatica.put("HR" , 32);
+        Empatica.put("ACC" , Acc);
+        Zephyr.put("BRR", Acc);
+        document.put("Empatica", Empatica);
+        document.put("Zephyr", Zephyr);
+        db.create(document);
+
     }
 }
